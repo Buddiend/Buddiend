e6c6b04ebce82460856d03d00e9db01fadc39435
Bojan Simichiev <bojansimiciev@gmail.com>
2022-03-14 18:05:24 +0100

    Init

---
+++ src/main/java/com/buddiend/buddiend/BuddiendApplication.java
@@ -0,0 +1,13 @@
+package com.buddiend.buddiend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuddiendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuddiendApplication.class, args);
+    }
+
+}
