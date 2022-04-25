5ab4b3448cd5d519f5e466466ff2632b12e24e9b
Bojan Simichiev <bojansimiciev@gmail.com>
2022-04-25 14:11:32 +0200

    Video Bug Fixes and Verify made through login

--- src/main/java/com/buddiend/buddiend/controllers/ExploreController.java
+++ src/main/java/com/buddiend/buddiend/controllers/ExploreController.java
@@ -4,6 +4,7 @@ import com.buddiend.buddiend.models.ChatRoom;
 import com.buddiend.buddiend.models.Topic;
 import com.buddiend.buddiend.services.ChatRoomService;
 import com.buddiend.buddiend.services.TopicService;
+import com.buddiend.buddiend.services.UserService;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,6 +13,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.mail.MessagingException;
+import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
 import java.util.*;
 
 @Controller
@@ -20,18 +24,27 @@ public class ExploreController {
 
     private final TopicService topicService;
     private final ChatRoomService chatRoomService;
+    private final UserService userService;
 
-    public ExploreController(TopicService topicService, ChatRoomService chatRoomService) {
+    public ExploreController(TopicService topicService, ChatRoomService chatRoomService, UserService userService) {
         this.topicService = topicService;
         this.chatRoomService = chatRoomService;
+        this.userService = userService;
     }
 
     @GetMapping
-    public String showExplorePage(Model model){
+    public String showExplorePage(HttpServletRequest request, Model model) throws MessagingException, UnsupportedEncodingException {
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         Object principal = authentication.getPrincipal();
         String email = ((UserDetails)principal).getUsername();
+
+        if(!this.userService.findByEmail(email).isEnabled()){
+            request.getSession().setAttribute("user_to_register", email);
+            this.userService.resendVerificationCode(email);
+            return "redirect:/verify";
+        }
+
         List<Topic> selectedTopics = this.topicService.findSelected(email);
         List<ChatRoom> chatRooms = this.chatRoomService.findByTopics(selectedTopics);
 
