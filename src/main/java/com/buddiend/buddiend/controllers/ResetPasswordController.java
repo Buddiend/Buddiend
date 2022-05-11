f7e9b855a55e37575f34bb74893cf200dcd03f03
Bojan Simichiev <bojansimiciev@gmail.com>
2022-05-11 12:50:33 +0200

    Fix a bug in the ResetPasswordController

--- src/main/java/com/buddiend/buddiend/controllers/ResetPasswordController.java
+++ src/main/java/com/buddiend/buddiend/controllers/ResetPasswordController.java
@@ -3,6 +3,9 @@ package com.buddiend.buddiend.controllers;
 import com.buddiend.buddiend.models.dto.ResetPasswordDto;
 import com.buddiend.buddiend.models.dto.UserRegisterDto;
 import com.buddiend.buddiend.services.AuthService;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -25,6 +28,13 @@ public class ResetPasswordController {
 
     @GetMapping("/{token}")
     public String showResetPasswordPage(@PathVariable String token, Model model) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null) {
+            return "redirect:/explore";
+        }
+
         boolean validated = this.authService.validateToken(token);
 
         if (validated) {
